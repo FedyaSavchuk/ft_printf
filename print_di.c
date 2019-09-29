ecd419f273c74302fff5d33914d7e00d963c2bda
VitalyDorozhkin <dorozhkin.vitaly@gmail.com>
2019-09-30 00:07:01 +0300

    zero&plus fix

--- print_di.c
+++ print_di.c
@@ -1,26 +1,20 @@
-
 #include "printf.h"
 
-int     print_di(long long int nbr)
+int	print_di(long long int nbr)
 {
 	char *snbr;
 	int c;
+
 	snbr = ft_itoa_base(nbr, 10, 'a');
 	c = g_flags->min_width - ft_strlen(snbr) - g_flags->plus;
-	if(c <= 0 && g_flags->space && !g_flags->plus && nbr >= 0)
+	if (c <= 0 && g_flags->space && !g_flags->plus && nbr >= 0)
 		ft_putchar(' ');
-	if(g_flags->minus)
-	{
-		if(g_flags->plus && nbr >= 0)
+	if (g_flags->plus && nbr >= 0)
 			ft_putchar('+');
+	if (g_flags->minus)
 		ft_putstr(snbr);
-	}
 	ft_putchars((g_flags->zero && !g_flags->minus) ? '0' : ' ', c);
-	if(!g_flags->minus)
-	{
-		if(g_flags->plus && nbr >= 0)
-			ft_putchar('+');
+	if (!g_flags->minus)
 		ft_putstr(snbr);
-	}
 	return (0);
 }
